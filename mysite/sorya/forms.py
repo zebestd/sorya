83e483d326f32a1a4dde572d9f94a84138b33b97
Tan <demirtasemlakinsaat@gmail.com>
2021-07-02 05:08:59 +0300

    first commit

---
+++ mysite/sorya/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django.forms import ModelForm
+from .models import *
+
+
+
+class SoruForm(ModelForm):
+    class Meta:
+         model = Soru
+         fields = ('isim', 'kategori', 'soru')
---
+++ mysite/sorya/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from . import views
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+path('', views.soryalist, name="home"),
+path('addsoru/', views.createSoru, name="create_soru"),
+] + static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
